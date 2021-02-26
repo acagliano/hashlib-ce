4c95a333d102e6224d1252d4c88a18f43a6b7c01
Anthony Cagliano <ac@clrhome.org>
2021-02-26 00:10:12 -0500

    Add notes for RSA

--- src/hashlib.h
+++ src/hashlib.h
@@ -176,6 +176,18 @@ void hashlib_sha256final(sha256_ctx *ctx, uint8_t* digest);
 // ######################
 // ###### RSA-256  ######
 // ######################
+//
+//	RSA-256, which I will dub "TI-RSA" is a lightweight implementation of RSA
+//	for the TI-84+ CE. It uses a 256-bit pair of keys to conduct asymmetric
+//	encrypted data transfers between a TI-84+ CE client and some remote host.
+//
+//	NOTE: 256-bit keys are NOT cryptographically secure for practical applications
+//	RSA standard requires at least 1024-bits. Nonetheless this is here as 
+//	proof-of-concept and a learning exercise for me (and beck, as I provided him
+//	the algorithm, and a C implementation and he's doing the ASM for it.
+//	DO NOT SENSITIVE DATA WITH THIS PROTOCOL
+//	better yet, don't send sensitive data with a calculator
+//
 
 // Init Context for RSA-256
 //
