d2f26b25a51dd45ee32fc53433ba42851271a07b
Anthony Cagliano <ac@clrhome.org>
2021-02-26 00:11:26 -0500

    left out a )

--- src/hashlib.h
+++ src/hashlib.h
@@ -184,7 +184,7 @@ void hashlib_sha256final(sha256_ctx *ctx, uint8_t* digest);
 //	NOTE: 256-bit keys are NOT cryptographically secure for practical applications
 //	RSA standard requires at least 1024-bits. Nonetheless this is here as 
 //	proof-of-concept and a learning exercise for me (and beck, as I provided him
-//	the algorithm, and a C implementation and he's doing the ASM for it.
+//	the algorithm, and a C implementation and he's doing the ASM for it.)
 //	DO NOT SENSITIVE DATA WITH THIS PROTOCOL
 //	better yet, don't send sensitive data with a calculator
 //
